459a2b427abd17367ce6d5c3a906776746c9c4f8
dsebesta <dsebesta@gmail.com>
2019-05-24 16:48:02 -0500

    added mysql and tested query

--- jobs/waterData.js
+++ jobs/waterData.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 const schedule = require("node-schedule");
 const url = "https://gis2.arlingtontx.gov/agsext2/rest/services/OpenData/OD_Table/MapServer/1/query?where=1%3D1&outFields=*&outSR=4326&f=json";
 const refreshRate = 30;
+const mysql_con  = require('../config/mysql_config.js');
 
 
 console.log('waterData running...');
@@ -11,9 +12,26 @@ console.log('waterData running...');
 const getData = async url => {
 	console.log('grabbing water data ' + getDateTime())
   try {
+
     const response = await axios.get(url);
     const data = response.data;
+
     data.features.map(res => console.log(res.attributes.PREMISEADDRESS + ", ARLINGTON, TX " + res.attributes.PREMISEZIP + " - WATER OFF DATE: " + res.attributes.WATEROFFDATE));
+
+
+    let addressValue = 'wheaton'
+    let compareRecordSql = 'SELECT * FROM water_arlington.addresses WHERE street LIKE "%wheaton%"';
+
+    mysql_con.query(compareRecordSql, (error, results, fields) => {
+    	if (error) {
+    		return console.error(error.message);
+  		}
+  		console.log(results);
+    })
+
+    mysql_con.end();
+
+
   } catch (error) {
     console.log(error);
   }
@@ -44,8 +62,15 @@ function getDateTime() {
 
 };
 
-var j = schedule.scheduleJob('*/1 * * * *', function(){
-  console.log('pulling data...');
+mysql_con.connect((err) => {
+	if (err) throw err;
+	console.log("connected to mysql!")
+})
+
+// var j = schedule.scheduleJob('*/1 * * * *', function(){
+//   console.log('pulling data...');
   getData(url);
-});
+// });
+
+
 
