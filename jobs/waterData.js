82602b21dff6835339408f4366b2036f5d393d06
dsebesta <dsebesta@gmail.com>
2019-05-26 20:15:17 -0500

    completed data download, comparing against existing DB, and adding new rows

--- jobs/waterData.js
+++ jobs/waterData.js
@@ -9,6 +9,23 @@ const database  = require('../config/mysql_config.js');
 console.log('waterData running...');
 
 
+function getDateTime() {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+    return year + "-" + month + "-" + day;
+};
+
+
+database.connect((err) => {
+	if (err) throw err;
+	console.log("connected to mysql!")
+});
+
+
 const getData = async url => {
 	let date = getDateTime();
 	console.log('grabbing water data ' + date)
@@ -18,72 +35,56 @@ const getData = async url => {
 	    const data = response.data;
 
     	// loop through data and print out addresses
-    	data.features.map(
+    	await data.features.map(
 	    	res => {
+
 	    		let streetAddress = res.attributes.PREMISEADDRESS;
 	    		let zipCode = res.attributes.PREMISEZIP;
 	    		let waterOffDate = res.attributes.WATEROFFDATE;
 	    		let compareRecordSql = 'SELECT * FROM water_arlington.addresses WHERE street LIKE ?';
-	    		database.query(compareRecordSql, streetAddress, (error1, results1, fields) => {
+
+	    		await database.query(compareRecordSql, streetAddress, (error1, results1, fields1) => {
 			    	if (error1) {
 			    		return console.error('error1:', error1.message);
 			  		}
 			  		if (results1.length == 0 ) {
-
 						let insertRecordSql = 'INSERT INTO water_arlington.addresses (street, zip, water_shut_off, import_date) VALUES (?, ?, ?, ?)'
-						database.query(insertRecordSql, [streetAddress, zipCode, waterOffDate, date], (error2, results2, fields) => {
+						let yearOff = waterOffDate.slice(-4);
+						reformatWaterOffDate = yearOff + '/' + waterOffDate.slice(0, waterOffDate.length - 5);
+						database.query(insertRecordSql, [streetAddress, zipCode, reformatWaterOffDate, date], (error2, results2, fields2) => {
 							if (error2) {
 			    				return console.error('error2:', error2.message);
 			  				}
 			  				else {
 			  					console.log('inserted record: ', streetAddress);
 			  				}
-
 						})
+			  		}
+			  		else {
+			  			console.log('record already exists');
 			  		}
 			  		
 			    })    
 
 	    	}
 	    )
+
+	    console.log('closing database');
+		database.end();
+
     } 
 
     catch (error) {
     	console.log(error);
   	}
-};
 
-
-
-
-
-
-
-function getDateTime() {
-  
-    var date = new Date();
-
-    var year = date.getFullYear();
-
-    var month = date.getMonth() + 1;
-    month = (month < 10 ? "0" : "") + month;
-
-    var day  = date.getDate();
-    day = (day < 10 ? "0" : "") + day;
-
-    return year + "-" + month + "-" + day;
 
 };
 
-database.connect((err) => {
-	if (err) throw err;
-	console.log("connected to mysql!")
-})
+getData(url);
 
-// var j = schedule.scheduleJob('*/1 * * * *', function(){
-//   console.log('pulling data...');
-  getData(url);
-// });
+
+
 
 
 
---
+++ jobs/waterData2.js
@@ -0,0 +1,111 @@
+const axios = require("axios");
+const schedule = require("node-schedule");
+const url = "https://gis2.arlingtontx.gov/agsext2/rest/services/OpenData/OD_Table/MapServer/1/query?where=1%3D1&outFields=*&outSR=4326&f=json";
+const refreshRate = 30;
+const mysqlConfig  = require('../config/mysqlConfig.js');
+const mysql = require('mysql');
+let arlingtonData = {};
+let arrStreetAddresses = [];
+// const testData = ['1128 wheaton ct', '5901 RISING MEADOW DR'];
+
+
+
+console.log('waterData2 running...');
+
+function getDate() {
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+    let day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+    return year + "-" + month + "-" + day;
+};
+
+function formatDate(date) {
+	const year = date.slice(-4);
+	const month = date.slice(0, 2);
+	const day = date.slice(3, 5);	
+	const formatted_date = year + '-' + month + '-' + day;
+	return formatted_date;
+}
+
+class Database {
+    constructor( config ) {
+        this.connection = mysql.createConnection( config );
+    }
+    query( sql, args ) {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.query( sql, args, ( err, rows ) => {
+                if ( err )
+                    return reject( err );
+                resolve( rows );
+            } );
+        } );
+    }
+    close() {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.end( err => {
+                if ( err )
+                    return reject( err );
+                resolve();
+            } );
+        } );
+    }
+}
+
+Database.execute = async function( config, callback ) {
+
+	const response = await axios.get(url);
+	arlingtonData = response.data.features;
+	arrStreetAddresses = arlingtonData.map(results => results.attributes.PREMISEADDRESS);
+	// console.log(arrStreetAddresses);
+
+    const database = new Database( config );
+    return callback( database ).then(
+        result => database.close().then( () => result ),
+        err => database.close().then( () => { throw err; } )
+    );
+};
+
+
+Database.execute( mysqlConfig, 
+	database => database.query( 'SELECT * FROM water_arlington.addresses')
+		.then( rows => {
+	        let arrSqlStreetAddresses = rows.map(res => res.street); // separate street addresses from SQL results
+	        let newShutOffs = arrStreetAddresses.filter((item) => !arrSqlStreetAddresses.includes(item)) // filter new arlington data against SQL results
+	        let newShutOffsWithData = arlingtonData.filter((item) => newShutOffs.includes(item.attributes.PREMISEADDRESS)) // add back in additional info
+	        return newShutOffsWithData;
+	    } )
+	    .then( differences => {
+	        // console.log(differences);
+	        const todaysDate = getDate();
+	        const formattedSqlData = differences.map((item) => [item.attributes.PREMISEADDRESS, item.attributes.PREMISEZIP, formatDate(item.attributes.WATEROFFDATE), todaysDate]);
+	        console.log(formattedSqlData);
+	        if (formattedSqlData.length == 0) {
+	        	return 'no new records';
+	        }
+	        return database.query( 'INSERT INTO water_arlington.addresses (street, zip, water_shut_off, import_date) VALUES ?', [formattedSqlData] )
+	    } )
+)
+.then( (msg) => {
+    console.log(msg)
+} ).catch( err => {
+    console.log('error: ', err)
+} );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
